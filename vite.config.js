d426f78d191097c8d03a7f45ca8c1d8e24ccdf2e
Clément Lequenne <clementlequenne1@gmail.com>
2024-06-07 17:46:58 +0200

    ouai

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,42 @@
-import { defineConfig } from "vite"
-import { resolve } from "path"
-import { glob } from "glob"
-import tailwindcss from "tailwindcss"
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { glob } from 'glob';
+import tailwindcss from 'tailwindcss';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
-const root = resolve(__dirname, '')
-const outDir = resolve(__dirname, 'dist')
-const htmlDir = resolve(__dirname, 'rc/src/html')
+const root = resolve(__dirname, '');
+const outDir = resolve(__dirname, 'dist');
+const htmlDir = resolve(__dirname, 'src/html');
 
-const htmlFiles = glob.sync(`${htmlDir}/*.html`).reduce((acc, file) => {
-    const key = file.replace(`${htmlDir}/`, '').replace('.html', '')
-    acc[key] = file
-    return acc
-  }, {})
+const htmlFiles = glob.sync(`${htmlDir}/*.html`);
 
-export default {
-    plugins: [
-        tailwindcss({
-          config: './tailwind.config.js'
-        })
-      ],
-  base: '/restaurant-css-framework/',
+const inputFiles = htmlFiles.reduce((acc, file) => {
+  const name = file.replace(`${htmlDir}/`, '').replace('.html', '');
+  acc[name] = file;
+  return acc;
+}, { index: resolve(__dirname, 'index.html') });
+
+export default defineConfig({
+  plugins: [
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: {
+          cssPath: '/assets/style-DJeIUuWn.css'
+        }
+      }
+    }),
+    tailwindcss({
+      config: './tailwind.config.js'
+    })
+  ],
+  base: '/',
   root,
   build: {
     outDir,
     emptyOutDir: true,
     rollupOptions: {
-        input: htmlFiles
+      input: inputFiles
     }
-  },
-  optimizeDeps: {
-    include: ['src/**/*.html']
   }
-}
+});
